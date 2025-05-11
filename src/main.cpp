1d04f7cf3ce2829953b4fff27c37725db1d68f96
CUBE <2825694277@qq.com>
2025-05-12 01:15:17 +0800

    添加launch

--- src/main.cpp
+++ src/main.cpp
@@ -14,14 +14,12 @@ int main(int argc, char **argv){
     std::string child_frame_id_ = "base_footprint";
     bool use_sim_time = true;
 
-    n.getParam("/decision/use_sim_time", use_sim_time);
     n.getParam("/decision/config_path", config_path);
     n.getParam("/decision/config_file_name", config_file_name);
     n.getParam("/decision/referee_topic", referee_topic_);
     n.getParam("/decision/frame_id", frame_id_);
     n.getParam("/decision/child_frame_id", child_frame_id_);
 
-    n.setParam("/use_sim_time", use_sim_time);
     DecisionConfig decision_config;
     decision_config.decision_config_path_ = config_path + "/" + config_file_name;
     decision_config.referee_topic_ = referee_topic_;
